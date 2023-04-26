c8f029db1abae3b3cc622e61c403116188b9905a
VincentNL <71412899+VincentNLOBJ@users.noreply.github.com>
2023-04-26 11:23:23 +0200

    Update Ninja_extractor.py

--- Ninja_extractor.py
+++ Ninja_extractor.py
@@ -1,95 +1,71 @@
-#----------------------------------
-# Ninja extractor v1.0     
-# By VincentNL 2023/04/25
-#
-#----------------------------------
-# Extract .NJ/.NJM/.NJL/.NJD files
-# From any uncompressed archive.
-#----------------------------------
-
 import os
+import sys
 import tkinter as tk
 from tkinter import filedialog
 
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+
 def search(pattern, current_file, app_dir, filename):
     with open(current_file, "rb") as f:
-
         data = f.read()
         offset = 0
         addr_list = []
 
-        if pattern == b'NJCM':
-            ext = '.nj'
-        elif pattern == b'NMDM':
-            ext = '.njm'
-        elif pattern == b'NJTL':
-            ext = '.njl'
-        elif pattern == b'NMD\\':
-            ext = '.njd'
+        ext = None
+        if pattern == b'NJCM': ext = '.nj'
+        elif pattern == b'NMDM': ext = '.njm'
+        elif pattern == b'NJTL': ext = '.njl'
+        elif pattern == b'NMD\\': ext = '.njd'
 
         while True:
             i = data.find(pattern, offset)
-            if i == -1: # no other file found
-                ttl_files = len(addr_list)
-
-                break
-
-            offset = i + len(pattern)  # move cursor after string match
+            if i == -1: break
+            offset = i + len(pattern)
             addr_list.append(i)
 
-        # extraction
-        for i in range (ttl_files):
-            f.seek(addr_list[i]+4)
-            size = int.from_bytes(f.read(4), byteorder='little')
+        for i, addr in enumerate(addr_list):
+            try:
+                f.seek(addr + 4)
+                size = int.from_bytes(f.read(4), byteorder='little')
 
-            new_file = os.path.join(app_dir, f'Extracted', filename, f'{filename[0:-4]}_{(str(i)).zfill(4)}{ext}')
+                new_file = os.path.join(app_dir, f'Extracted/{filename}/{filename[0:-4]}_{str(i).zfill(4)}{ext}')
+                POF_OFFSET = f.seek(addr + 8 + size)
+                POF_CHECK = int.from_bytes(f.read(3), byteorder='little')
 
-            # check if POF at the end of size
-            POF_OFFSET = f.seek(addr_list[i]+8+size)
-            POF_CHECK = int.from_bytes(f.read(3), byteorder='little')
+                if POF_CHECK == 4607824:  # "POF" in decimal value
 
+                    f.seek(POF_OFFSET + 4)
+                    POF_SIZE = int.from_bytes(f.read(4), byteorder='little')
+                    FILE_END_OFFSET = POF_OFFSET + POF_SIZE + 8
+                    f.seek(addr)
+                    fsize = FILE_END_OFFSET - addr
 
-            if POF_CHECK != 4607824:
-                print('invalid')
-            # invalid file
+                    if not os.path.exists(os.path.join(app_dir, 'Extracted', filename)):
+                        os.makedirs(os.path.join(app_dir, 'Extracted', filename))
 
-            else:
-                # extract
-                f.seek(POF_OFFSET + 4)
-                POF_SIZE = int.from_bytes(f.read(4), byteorder='little')
+                    with open(new_file, "wb") as n:
+                        n.write(f.read(fsize))
+            except: print(f'{current_file}\nNinja model file error!')
 
-
-                FILE_END_OFFSET = POF_OFFSET+POF_SIZE+8
-                f.seek(addr_list[i])
-                fsize = FILE_END_OFFSET-addr_list[i]
-
-
-                if not os.path.exists(os.path.join(app_dir, 'Extracted', filename)):
-                    os.makedirs(os.path.join(app_dir, 'Extracted', filename))
-
-                with open(new_file, "wb") as n:
-                    n.write(f.read(fsize))
-
-# ---------
-# Main Loop
-# ---------
 
 # Open a file dialog to select the input file(s)
+iconPath = resource_path('ninja.ico')  # window icon
 root = tk.Tk()
 root.withdraw()
+root.iconbitmap(iconPath)
 
-my_file = filedialog.askopenfilenames(
-    initialdir=".",
-    title="Select file(s) to scan",
-    filetypes=[("*.*", "*.*")]
-)
+my_files = filedialog.askopenfilenames(initialdir=".", title="Select file(s) to scan", filetypes=[("*.*", "*.*")])
 
-for filename in my_file:
-    current_file = filename
-    source_dir, filename = os.path.split(filename)
-
-    app_dir = os.path.abspath(os.path.join(os.getcwd()))
-    search(b'NJCM', current_file, app_dir, filename)
-    search(b'NMDM', current_file, app_dir, filename)
-    search(b'NJTL', current_file, app_dir, filename)
-    search(b'NMD\\', current_file, app_dir, filename)
+# Main Loop
+for file in my_files:
+    current_file = file
+    source_dir, filename = os.path.split(file)
+    app_dir = os.path.abspath(os.getcwd())
+    for pattern in [b'NJCM', b'NMDM', b'NJTL', b'NMD\\']:
+        search(pattern, current_file, app_dir, filename)
